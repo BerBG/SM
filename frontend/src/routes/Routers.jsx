4624727092e16014876965d925abf1fd01b9ba05
BerBG <bgferreira0@gmail.com>
2024-04-11 18:04:53 -0300

    projeto incial

---
+++ frontend/src/routes/Routers.jsx
@@ -0,0 +1,24 @@
+import Home from "../pages/Home";
+import Services from "../pages/Services";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Contact from "../pages/Contact";
+import Tutors from "../pages/Tutors/Tutors";
+import TutorsDetails from "../pages/Tutors/TutorDetails";
+
+import { Routes, Route } from "react-router-dom";
+
+const Routers = () => {
+  return <Routes>
+    <Route path="" element={<Home />} />
+    <Route path="/home" element={<Home />} />
+    <Route path="/tutors" element={<Tutors />} />
+    <Route path="/tutors:id" element={<TutorsDetails />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Signup />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/services" element={<Services />} />
+  </Routes>;
+};
+
+export default Routers;
