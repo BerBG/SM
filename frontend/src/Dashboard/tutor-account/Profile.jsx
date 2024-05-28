cfa90d5e74a84583a8aeb02f82448573c327ae24
BerBG <bgferreira0@gmail.com>
2024-05-28 17:26:30 -0300

    profile

--- frontend/src/Dashboard/tutor-account/Profile.jsx
+++ frontend/src/Dashboard/tutor-account/Profile.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { useState } from "react";
 
 const Profile = () => {
-  return <div>
-    <h2 className='text-headingColor font-bold text-[24px] leading-9 mb-10'>Profile Information</h2>
-  </div>
-}
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+  });
 
-export default Profile
+  const handleInputChange = (e) => {};
+
+  return (
+    <div>
+      <h2 className="text-headingColor font-bold text-[24px] leading-9 mb-10">
+        Profile Information
+      </h2>
+
+      <form>
+        <div className="mb-5">
+          <p className="form__label">Name*</p>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            placeholder="Full Name"
+            className="form__input"
+          />
+        </div>
+        <div className="mb-5">
+          <p className="form__label">Email*</p>
+          <input
+            type="text"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            placeholder="Email"
+            className="form__input"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Profile;
